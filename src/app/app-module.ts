882f8f7ab9dba03ef4fe50a65c700b5e7a8856e4
angeldzz <pintodiazangel@gmail.com>
2025-11-05 10:56:43 +0100

    formularios sin referencias con binding y FormsModule

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -6,6 +6,8 @@ import { PrimerComponent } from '../components/primercomponent/primer.component'
 import { HooksAngular } from '../components/hooksangular/hooksangular.component';
 import { Deportes } from '../components/deportes/deportes.component';
 import { DeportesV2Component } from '../components/deportesv2/deportesv2.component';
+import { FormsModule } from '@angular/forms';
+import { FormBindingComponent } from '../components/formbinding/formbinding.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,11 @@ import { DeportesV2Component } from '../components/deportesv2/deportesv2.compone
     PrimerComponent,
     HooksAngular,
     Deportes,
-    DeportesV2Component
+    DeportesV2Component,
+    FormBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- src/components/deportesv2/deportesv2.component.ts
+++ src/components/deportesv2/deportesv2.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 
+
 @Component({
     selector: 'app-deportesv2',
     templateUrl: './deportesv2.component.html',
