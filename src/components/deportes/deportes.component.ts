62bbbc8a8c6aee79df0af54c6ad2c67d5243e749
angeldzz <pintodiazangel@gmail.com>
2025-11-05 10:00:42 +0100

    condicionales css

--- src/components/deportes/deportes.component.ts
+++ src/components/deportes/deportes.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-deportes',
     templateUrl: './deportes.component.html',
+    styleUrl:'./deportes.component.css',
     standalone:false,
 })
 
 export class Deportes implements OnInit {
     public sports: Array<string>
+    public numeros: Array<number>
     constructor() { 
         this.sports = ["Canicas", "Curling", "Dardos", "Petanca","Tenis"]
+        this.numeros = [9,8,7,6,5,4,3,2,1]
     }
 
     ngOnInit():void { 
