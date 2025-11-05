e764225e04bc11615f31b2746d3deeaa360723ac
angeldzz <pintodiazangel@gmail.com>
2025-11-05 10:27:50 +0100

    DIRECTIVAS ANGULAR

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,6 +5,7 @@ import { App } from './app';
 import { PrimerComponent } from '../components/primercomponent/primer.component';
 import { HooksAngular } from '../components/hooksangular/hooksangular.component';
 import { Deportes } from '../components/deportes/deportes.component';
+import { DeportesV2Component } from '../components/deportesv2/deportesv2.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { Deportes } from '../components/deportes/deportes.component';
     PrimerComponent,
     HooksAngular,
     Deportes,
+    DeportesV2Component
   ],
   imports: [
     BrowserModule
---
+++ src/components/deportesv2/deportesv2.component.ts
@@ -0,0 +1,17 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'app-deportesv2',
+    templateUrl: './deportesv2.component.html',
+    styleUrl:"./deportesv2.component.css",
+    standalone:false
+})
+
+export class DeportesV2Component {
+    public sports: Array<string>
+    public numeros: Array<number>
+    constructor() { 
+        this.sports = ["Petanca", "Futbol", "Tenis", "Baloncesto"]
+        this.numeros = [9,8,7,6,5,4,3,2,1]
+    }
+}
